27d87af470f0ab4f04acd437cab0e6e1b4f31e34
Jacob <j.chislett@mail.utoronto.ca>
2026-04-02 10:53:32 -0400

    added instructions for academic calendar dataset generation

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ from TreeVisualizer import TreeVisualizer
 if __name__ == "__main__":
     visualizer = TreeVisualizer()
     visualizer.run_simulation()
+
+    # |----------------------------------------------------------------------------------------------------------------|
+    # | UNCOMMENT THE BELOW CODE FOR A DEMONSTRATION ON HOW THE ACADEMIC CALENDAR DATASET IS GENERATED                 |
+    # | This code will scrape information from the U of T academic calendar website, parse the prerequisite strings,   |
+    # | rewrite them in a format that is easier to run computations on and save all information including course names |
+    # | and descriptions to a json file called 'dataset_example.json'                                                  |
+    # |----------------------------------------------------------------------------------------------------------------|
+    # from academic_calendar_reader import PrerequisiteTreeLoader
+    # loader = PrerequisiteTreeLoader
+    # loader.load_from_programs(['Computer Science'])
+    # loader.save_to_file('dataset_example.json')
