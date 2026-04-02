a73b38eb87c34580459eaa5e4d174e8d4df26619
Shuhan6017 <shuhan200702@gmail.com>
2026-04-02 02:18:49 -0400

    insert docstring

--- web_element_helper.py
+++ web_element_helper.py
@@ -1,4 +1,4 @@
-"""Selenium web element utilities for robust finding, waiting, and stale checking.
+"""Selenium web element utilities for robust finding, waiting, and stale checking
 
 This file is Copyright (c) 2026 Shayan Bhatti, Jacob Chislett, Ethan Diep, Shuhan Yuan
 """
